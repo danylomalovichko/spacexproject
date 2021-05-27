2538e09d074d09b7a74f29f7fc3c959ba54bb4f2
Danylo Malovichko <ovrhro@gmail.com>
2021-05-27 22:32:07 +0300

    Add pull to refresh

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,38 +14,52 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     private let networkManager = NetworkManager()
     private var dragons: [Dragon] = []
     private var selectedDragon: Dragon?
+    private let pullToRefresh = UIRefreshControl()
     
     let cellReuseIdentifier = "DragonCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.backgroundView = pullToRefresh
+        pullToRefresh.addTarget(self, action: #selector(loadDragonList), for: .valueChanged)
+        tableView.refreshControl = pullToRefresh
+        
         tableView.register(DragonCell.self, forCellReuseIdentifier: cellReuseIdentifier)
         let nib = UINib.init(nibName: cellReuseIdentifier, bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: cellReuseIdentifier)
         
-        // networkManager.getDragon(id: "5e9d058759b1ff74a7ad5f8f")
-        
-        networkManager.getDragonList { dragonsResult in
-            debugPrint(dragonsResult)
-            self.dragons = dragonsResult
-            self.tableView.reloadData()
-        }
+        loadDragonList()
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDragonDetails" {
             let vc = segue.destination as! DragonDetailsViewController
             vc.dragon = selectedDragon
         }
     }
+    
+    @objc func loadDragonList() {
+        
+        networkManager.getDragonList { dragonsResult in
+            debugPrint(dragonsResult)
+            self.dragons = dragonsResult
+            self.tableView.reloadData()
+            self.pullToRefresh.endRefreshing()
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dragons.count
     }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! DragonCell
         cell.setup(dragon: dragons[indexPath.row])
         return cell
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
         selectedDragon = dragons[indexPath.row]
